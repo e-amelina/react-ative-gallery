b603a05c507e33d88ecb4a55277a3baa34c08760
Elena Amelina <e.amelina20@gmail.com>
2021-03-22 08:30:44 +0200

    Added styles for modal

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,35 +1,53 @@
-import { Text, View, StyleSheet,Image } from "react-native";
+import { Text, View, StyleSheet,Image, Dimensions } from "react-native";
 import React from 'react';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export function Modal({route}: any) {
     console.log(route);
     
     
-    const {title, url} = route.params;
+    const {pictureTitle, pictureUrl} = route.params;
    return (
     <View style={styles.modal}>
         <Text style={styles.imageName}>
-            {title}
+            {pictureTitle}
         </Text>
-        <Image
-            style={styles.image}
-            source={{
-                uri: url
-            }}
-        />
+        <TouchableOpacity  
+                  style={styles.wrapImg}
+                  activeOpacity = { .5 } 
+                  >
+            <Image
+                style={styles.image}
+                source={{
+                    uri: pictureUrl
+                }}
+            />
+        </TouchableOpacity>
     </View>
    ) 
     
 }
 
 const styles = StyleSheet.create({
+    wrapImg: {
+        // height: Dimensions.get('window').height,
+        height: 350,
+        width: 350,
+        // margin: 1,
+        // padding: 1
+        alignSelf: 'center'
+    },
     modal: {
         flex: 1,
         padding: 40,
         backgroundColor: 'rgba(0, 0, 0, 0.8)'
     },
     imageName: {
-        color: '#fff'
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'bold',
+        padding: 20
     },
     image: {
         alignSelf: 'stretch',
